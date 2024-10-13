85fe88a66129dcb71b21074772101d9f46d7cb4c
DaniloLecca <d.lecca39@studenti.unica.it>
2024-10-13 12:13:30 +0200

    Esercizio completato

---
+++ 1/exam-committee/exam-committee.ml
@@ -0,0 +1,14 @@
+type vote = StrongAccept | StrongReject | WeakAccept | WeakReject
+
+let rejected a = match a with
+| StrongReject -> true
+| _ -> false;;
+
+let decide_exam a b c = match (a, b, c) with
+| _ when rejected a || rejected b || rejected c -> false
+| (WeakReject, WeakReject, _) -> false
+| (WeakReject, _, WeakReject) -> false
+| (_, WeakReject, WeakReject) -> false
+| _ -> true;;
+
+decide_exam WeakReject WeakReject WeakAccept;;
